0952ad73d1e7bdf2f9e57d2fb1b00f7009a97736
Arshia <arshiamoghaddam93@gmail.com>
2024-05-29 16:38:02 -0700

    Added contact me icons

--- app/page.tsx
+++ app/page.tsx
@@ -90,7 +90,38 @@ export default function Page() {
           , scratchpad, and outdoorsy.
         </li>
       </ul>
-      {/* place icon with arrow to email me or view my LinkedIn */}
+      <ul className="font-sm mt-8 flex flex-col md:flex-row space-x-0 space-y-2 md:space-x-4 md:space-y-0 text-neutral-600 dark:text-neutral-200 ">
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            target="_blank"
+            href="https://www.linkedin.com/in/arshiamog/"
+          >
+            <Image
+              src="/icons/arrow-up-right.svg"
+              width={15}
+              height={15}
+              alt="Up arrow"
+            />
+            <p className="ml-2">Connect with me</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            target="_blank"
+            href="mailto:arshiamoghaddam93@gmail.com"
+          >
+            <Image
+              src="/icons/arrow-up-right.svg"
+              width={15}
+              height={15}
+              alt="Up arrow"
+            />
+            <p className="ml-2">Email me</p>
+          </a>
+        </li>
+      </ul>
       {/* Built with coffee in SF/SD  */}
     </section>
   );
